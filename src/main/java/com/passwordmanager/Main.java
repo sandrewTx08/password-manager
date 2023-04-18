57a25ab096b449179463cba36178811ad4a5874b
sandrewtx08 <sandrewtx08@protonmail.com>
2023-04-18 12:21:59 -0300

    fix: env variables

--- src/main/java/com/passwordmanager/Main.java
+++ src/main/java/com/passwordmanager/Main.java
@@ -2,12 +2,10 @@ package com.passwordmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 @EnableMongoRepositories
-@PropertySource("file:.env")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
