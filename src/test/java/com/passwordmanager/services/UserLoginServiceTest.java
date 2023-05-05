dc8f04440c87b1dfdb975f57c7da981a71881e01
sandrewtx08 <sandrewtx08@protonmail.com>
2023-05-05 15:13:50 -0300

    chore: mongo test & ci

--- src/test/java/com/passwordmanager/services/UserLoginServiceTest.java
+++ src/test/java/com/passwordmanager/services/UserLoginServiceTest.java
@@ -1,50 +1,59 @@
 package com.passwordmanager.services;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.when;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.bson.types.ObjectId;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import com.passwordmanager.models.Login;
 import com.passwordmanager.models.User;
-import com.passwordmanager.repositories.UserLoginRepository;
-import com.passwordmanager.repositories.UserRepository;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class UserLoginServiceTest {
-    @InjectMocks
-    UserLoginService userLoginService;
+    @Autowired
+    private UserLoginService userLoginService;
 
-    @Mock
-    UserRepository userRepository;
+    @Autowired
+    private MongoTemplate mongoTemplate;
 
-    @Mock
-    UserLoginRepository userLoginRepository;
+    private final Login login = new Login();
+    private final User user = new User();
 
-    @Test
-    public void findUserLogins() {
-        User user = new User();
+    @BeforeAll
+    public void beforeAll() {
         user.set_id(new ObjectId());
         user.setEmail("login@example.com");
         user.setPassword("password123");
 
-        Login login = new Login();
+        mongoTemplate.save(user);
+
         login.setUser(user);
         login.setWebsite("https://example.com");
         login.setPassword("admin123");
 
-        when(userRepository.findById(user.get_id())).thenReturn(Optional.of(user));
-        when(userLoginRepository.findUserLogins(user.get_id())).thenReturn(List.of(login));
+        mongoTemplate.save(login);
+    }
 
+    @AfterAll
+    public void afterAll() {
+        mongoTemplate.remove(user);
+        mongoTemplate.remove(login);
+    }
+
+    @Test
+    public void findUserLogins() {
         List<Login> userLogins = userLoginService.findUserLogins(user.get_id());
 
-        assertEquals(login, userLogins.get(0));
+        assertEquals(login.get_id(), userLogins.get(0).get_id());
     }
 }
